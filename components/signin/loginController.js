e6be1ceb4167a908b8f34e15676338a4eab5aace
remymuhire <jeanremy1806@gmail.com>
2017-11-12 20:30:09 +0200

    loginCtrl

--- components/signin/loginController.js
+++ components/signin/loginController.js
@@ -1,5 +1,12 @@
-save.controller('signinCtrl', function($scope, $location,AuthService){
+save.controller('signinCtrl', function($scope, $location, AuthService){
     $scope.signin = function(){
         console.log($scope.user);
+        AuthService.signin(JSON.stringify($scope.user))
+                   .then(function(){
+                       console.log("login");
+                   })
+                   .catch(function(){
+                       console.log("not login");
+                   });
     }
 })
