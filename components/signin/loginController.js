2e9fdf37a495b6decaf9c6f09a3f2db9bf1c886e
remymuhire <jeanremy1806@gmail.com>
2017-12-05 10:37:52 +0200

    login controller

--- components/signin/loginController.js
+++ components/signin/loginController.js
@@ -1,8 +1,9 @@
 save.controller('signinCtrl', function($scope, $location, AuthService){
     $scope.signin = function(){
         console.log($scope.user);
-        AuthService.signin(JSON.stringify($scope.user))
+        AuthService.sign_in(JSON.stringify($scope.user))
                    .then(function(){
+                       $location.path("/project")
                        console.log("login");
                    })
                    .catch(function(){
