d2c7e2806ec26e88d88e5cda1dd1429c2ab45aba
remymuhire <jeanremy1806@gmail.com>
2017-12-05 10:38:10 +0200

    login service

--- components/signin/loginService.js
+++ components/signin/loginService.js
@@ -1,35 +1,61 @@
-save.factory('AuthService', ['$base64','$q','$timeout','$http', function($q, $timeout, $http, $bas64){
+save.factory('AuthService', ['$cookieStore','$base64','$q','$timeout','$http', function($cookieStore, $base64, $q, $timeout, $http){
     // create user variable
     var user = null;
+    // create new instance for deffered
+    var deferred = $q.defer();
     var url = 'http://api.getsave.io:5000/v1/';
     return ({
-       signin: signin 
-    });
-
-    function isSignedIn(){
-        if (user){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    
-    function signin(data){
-        // create new instance for deffered
-        var deferred = $q.defer();
-
-        $http.post(url+'login/', data).succcess(function(data, status){
-            if (status == 200){
+        sign_in: function (data){
+            $http.post(url+'login/', data)
+                 .then(function successCallback(response){
+                     user = true;
+                     console.log(response.data.id);
+                     deferred.resolve();
+                 }, function errorCallback(response){
+                     user =  false;
+                     deferred.reject();
+                 });
+            return deferred.promise;
+        },
+        is_signed_in: function (){
+            if (user){
+                return true;
+            }else{
+                return false;
+            }
+        },
+        get_user_status: function(){
+            if (get_user_cookies()){
                 user = true;
+            }else{
+                user = false;
+            }
+            return user;
+        },
+        sign_out: function(){
+            if (remove_user_cookies()){
+                user = false;
                 deferred.resolve();
             }else{
                 user = false;
                 deferred.reject();
             }
-        }).error(function(){
-            user = false;
-            deferred.reject();
-        });
+            return deferred.promise;
+        }
+      
+    });
+
+    function set_user_cookies(user_id){
+        $cookieStore.put('__save',user_id);
     }
+
+    function get_user_cookies(){
+        return $cookieStore.get('__save');
+    }
+
+    function remove_user_cookies(){
+        $cookieStore.remove('__save');
+    }
+     
     
 }]);
